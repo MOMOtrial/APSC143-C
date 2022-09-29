c3022c8711f819a42a39551f0930bf9e3b20bba7
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-09-29 14:31:25 -0400

    Create main.c

---
+++ Variables/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+// Variable sub in --> format specifiers
+// %d = intergers (int)
+// %f = float  %[width].[precision][type]
+//%c = characters (char)
+int main() {
+    float height = 16;
+    int age = 32;
+    printf("Height is %f and age is %d\n", height, age);
+
+    float y = 4.83;
+    printf("\tThe value of y is %8.2f", y);
+
+    char example = '\"';
+    printf("%c", example);
+}
+
+// Addition -> +
+// Subtraction -> -
+// Multiplication -> *
+// Division -> /
+// Modulus -> % gives remainder of division
+
+int main() {
+    int y = 5;
+    y += 5;
+    printf("%d", y);
+}
+
+int main() {
+    int radius = 4;
+    float area = PI * pow(radius, 2);
+    printf("The area pf the circle is %f", area);
+}
