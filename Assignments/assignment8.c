98e8c7b4769f6b0b25d89eaaa17bb083da3abd31
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-11-16 22:23:50 -0500

    Update assignment8.c

--- Assignments/assignment8.c
+++ Assignments/assignment8.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+e <stdio.h>
 #include <math.h>
 
-double getUserInput();
-double getRiemannSum(double x, double y, int a);
+// Define function
+double error(double x, double y, int a);
 double getTaylorSum(double x, double y, int a);
-double getTaylor(double x, int total);
-double error(double x, double y, double a);
-int factorial(int x);
+double getRiemannSum(double x, double y, int a);
+double getUserInput();
+double getTaylor(double v, int total);
+int factorial(int num);
 
+// Print output to terminal
 int main() {
     printf("Enter the bounds to compute the Riemann Sum:");
     double leftBound = getUserInput();
@@ -24,18 +26,28 @@ int main() {
     printf("Taylor Series Riemann Sum with 25 intervals: %0.2lf\n", getTaylorSum(leftBound, rightBound, 25));
     printf("Taylor Series Riemann Sum with 100 intervals: %0.2lf\n", getTaylorSum(leftBound, rightBound, 100));
     printf("--------------------------------------------------------\n");
-    printf("Error in Riemann Sums with 5 intervals: -%0.2lf\n", error(leftBound, rightBound, 5));
-    printf("Error in Riemann Sums with 25 intervals: -%0.2lf\n", error(leftBound, rightBound, 25));
-    printf("Error in Riemann Sums with 100 intervals: -%0.2lf\n", error(leftBound, rightBound, 100));
+    printf("Error in Riemann Sums with 5 intervals: -%0.2lf%\n", error(leftBound, rightBound, 5));
+    printf("Error in Riemann Sums with 25 intervals: -%0.2lf%\n", error(leftBound, rightBound, 25));
+    printf("Error in Riemann Sums with 100 intervals: -%0.2lf%\n", error(leftBound, rightBound, 100));
 }
 
-
+// Collect User input
 double getUserInput() {
     double userValue;
     scanf("%lf", &userValue);
     return userValue;
 }
 
+// Get taylor approx
+double getTaylor(double v, int total) {
+    double taylor = 1;
+    for (int i = 1; i < total; i += 1) {
+        taylor = taylor + (pow((pow(v, 2)), i))/factorial(i);
+    }
+    return taylor;
+}
+
+// Get riemann sum
 double getRiemannSum(double x, double y, int a) {
     double array[a];
     double xi;
@@ -51,14 +63,7 @@ double getRiemannSum(double x, double y, int a) {
     return sum;
 }
 
-double getTaylor(double x, int total) {
-    double taylor = 1;
-    for (int i = 1; i < total; i += 1) {
-        taylor = taylor + (pow((pow(x, 2)), i))/factorial(i);
-    }
-    return taylor;
-}
-
+// Get taylor sum
 double getTaylorSum(double x, double y, int a) {
     double array[a];
     double xi;
@@ -74,20 +79,22 @@ double getTaylorSum(double x, double y, int a) {
     return sum;
 }
 
-int factorial(int x) {
-    int fact = x;
-    if (x==0) {
-        fact = 1;
-    }else {
-        for(int i = x - 1; i > 0; i += 1) {
-            fact = fact + 1;
-        }
-    }
-    return fact;
-}
-
-double error(double x, double y, double a) {
+// Calculate error
+double error(double x, double y, int a) {
     double error = 1 - getTaylorSum(x, y, a)/ getRiemannSum(x, y, a);
     error = 100*error;
     return error;
+}
+
+// Calculate factorial
+int factorial(int num) {
+    int fact = num;
+    if (num==0) {
+        fact = 1;
+    }else {
+        for(int i = num - 1; i > 0; i -= 1) {
+            fact = fact*i;
+        }
+    }
+    return fact;
 }
